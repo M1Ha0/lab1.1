f5630f18cb6bf2717b2a0b99cf57144f8f46420d
пк <@DESKTOP-81SKH73>
2024-10-10 16:47:14 +0200

    Добавьте файлы проекта.

---
+++ lab1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+    Console.WriteLine("Введите a");
+    double a = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите b");
+    double b = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите x");
+    double x = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите z");
+    double z = double.Parse(Console.ReadLine());
+    double c = Math.Pow(Math.Cos(b * Math.Pow(x, 5)), 7) - (Math.Sin(a * a) + Math.Cos(Math.Pow(x, 3) + Math.Pow(z, 5) - Math.Pow(a, 2)));
+    double v = Math.Asin(Math.Pow(a, 2)) + Math.Acos(Math.Pow(x, 7) - Math.Pow(a, 2));
+    Console.WriteLine($"f={c / v}");
+}
+catch
+{
+    Console.WriteLine("Введите корректные данные");
+}
